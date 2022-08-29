39a273b369d55e8161af7618da2bb95b77ae4fda
Vinicius Marques <vdcm.ecp19@uea.edu.br>
2022-08-28 21:41:52 -0400

    primeiro commit

---
+++ src/modelo/entidades/OrdemItem.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.entidades;
+
+/**
+ *
+ * @author Acer
+ */
+public class OrdemItem {
+    private Integer quantity;
+	private Double price;
+	
+	private Produto product;
+
+	public OrdemItem(Integer quantity, Double price, Produto product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Produto getProduct() {
+		return product;
+	}
+
+	public void setProduct(Produto product) {
+		this.product = product;
+	}
+
+	public double subTotal() {
+		return price * quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return product.getName() 
+				+ ", $" 
+				+ String.format("%.2f", price) 
+				+ ", Quantidade: " 
+				+ quantity + 
+				", Subtotal: $" 
+				+ String.format("%.2f", subTotal());
+	}
+}
